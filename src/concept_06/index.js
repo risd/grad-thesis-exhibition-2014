39794a8b801b3da906e0ef1bc28b6cb70672ff4d
rubillionaire <mail@rubenrodriguez.me>
2014-04-24 19:05:50 -0400

    multi color
    random colors are loaded. a seperate colors is used for the background screen on the about overlay

--- src/concept_06/index.js
+++ src/concept_06/index.js
@@ -2,7 +2,15 @@ var Nav = require('./overlay_nav');
 
 module.exports = function site () {
     var self = {},
-        colors = ['purple', 'pink'];
+        color_values = {
+            purple: 'rgb(38, 34, 98);',
+            orange: 'rgb(255, 61, 56);',
+            'lt-purple': 'rgb(146, 53, 125)',
+            green: 'rgb(144, 218, 73)',
+            blue: 'rgb(43, 89, 184)'
+        };
+
+    var colors = Object.keys(color_values);
 
     var nav = Nav();
 
@@ -12,10 +20,22 @@ module.exports = function site () {
         var body = d3.select('body');
         body.html('');
 
-        var color = colors[Math.floor(Math.random() * colors.length)];
+        var random_index = Math.floor(Math.random() * colors.length);
+
+        var color = colors[random_index];
+        var alt_colors = colors.slice(0,random_index)
+                               .concat(colors.slice(
+                                                random_index + 1,
+                                                colors.length));
+
+        var alt_color = alt_colors[
+                            Math.floor(
+                                Math.random() *
+                                alt_colors.length)];
 
         body.classed('concept_06', true);
-        body.classed(color, true);
+        body.classed('body-' + color, true);
+        body.classed('body-alt-' + alt_color, true);
 
         d3.html("http://" +
                 window.location.host +
@@ -29,6 +49,8 @@ module.exports = function site () {
                 .datum(function () { return this.dataset; });
 
             nav.targetActivatePairs(pairs)
+                .rotateBackground(
+                        d3.selectAll('.rotating-background-image'))
                 .setup();
         });
         return self;
--- src/concept_06/overlay_nav.js
+++ src/concept_06/overlay_nav.js
@@ -1,7 +1,10 @@
 module.exports = function nav () {
     var self = {},
         target_activate_pairs = [],
-        rotate_background_sel;
+        rotate_background_sel,
+        rotate_background_length = 0,
+        rotate_direction_ascending = true;
+        overlaid = false;
 
     self.targetActivatePairs = function (_) {
         if (!arguments.length) return target_activate_pairs;
@@ -12,26 +15,87 @@ module.exports = function nav () {
     self.rotateBackground = function (_) {
         if (!arguments.length) return rotate_background_sel;
         rotate_background_sel = _;
+
+        // set intial values;
+        rotate_background_sel
+            .datum(function (d, i) {
+                d = {};
+
+                d.opacity = (i === 0) ? 1 : 0;
+                rotate_background_length += 1;
+
+                return d;
+            });
+
         return self;
     };
 
     self.setup = function () {
         if (!target_activate_pairs) throw "requires elements to pair";
         target_activate_pairs
-            .on('click.nav', function (d, di) {
-                console.log('clicked nav');
+            .on('click.openNav', function (d, di) {
+                overlaid = true;
                 var to_activate = d3.select(d.activate);
                 
-                to_activate.classed('overlaid', true);
+                to_activate.classed('overlaid', overlaid);
+                if (rotate_background_sel) rotate();
             });
         target_activate_pairs.each(function (d, i) {
             var to_activate = d3.select(d.activate);
 
-            to_activate.on('click.nav', function () {
-                d3.select(this).classed('overlaid', false);
+            to_activate.on('click.closeNav', function () {
+                overlaid = false;
+                d3.select(this).classed('overlaid', overlaid);
             });
         });
     };
+
+    function rotate () {
+        console.log('rotating');
+        rotate_background_sel.transition()
+            .duration(5000)
+            .style('opacity', function (d) {
+                return d.opacity;
+            })
+            .each("end", function () {
+                // find current 
+                var current_index = 0,
+                    next_current_index;
+
+                // get the current index
+                rotate_background_sel.each(function (d, i) {
+                    if (d.opacity === 1) {
+                        current_index = i;
+                    }
+                });
+
+                // set the next index based on ascending or not
+                // also changing ascending bool if necessary
+                if (rotate_direction_ascending) {
+                    next_current_index = current_index + 1;
+                    if (next_current_index >
+                         (rotate_background_length - 1)) {
+                        current_index =
+                            rotate_background_length - 1;
+                        rotate_direction_ascending = false;
+                    }
+                } else {
+                    next_current_index = current_index - 1;
+                    if (next_current_index < 0) {
+                        next_current_index = 0;
+                        rotate_direction_ascending = true;
+                    }
+                }
+
+                // set opacity values based on next current index
+                rotate_background_sel.each(function (d, i) {
+                    d.opacity = (i === next_current_index) ?
+                                1 : 0;
+                });
+
+                if (overlaid) rotate();
+            });
+    }
 
     return self;
 };
