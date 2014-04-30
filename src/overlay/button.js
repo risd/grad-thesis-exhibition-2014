e4e00c46b6fc46e7b0f356063454d156fc14f581
rubillionaire <mail@rubenrodriguez.me>
2014-04-30 17:08:59 -0400

    remove js rotate

--- src/overlay/button.js
+++ src/overlay/button.js
@@ -1,25 +1,18 @@
-var RotateSvg = require('./rotate');
-
 module.exports = function button () {
     var self = {},
         selection,
         dimensions;
-
-    var rotate_svg = RotateSvg();
 
     self.selection = function (_) {
         if (!arguments.length) return selection;
         selection = _;
 
         dimensions = get_dimensions(selection);
-        rotate_svg
-            .selection(selection.select('#flower'));
 
         return self;
     };
 
     self.start = function () {
-        // rotate_svg.start();
         return self;
     };
 
