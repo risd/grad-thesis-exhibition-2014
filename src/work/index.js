722d789466a27340f6c2c7dcc0e574539a59e288
rubillionaire <mail@rubenrodriguez.me>
2014-05-13 11:17:34 -0400

    ga tracking
    added for when a user hits the bottom of the window to load more data

--- src/work/index.js
+++ src/work/index.js
@@ -43,6 +43,14 @@ module.exports = function work () {
 
             // update the filterable list
             departments.isFilterable(transformed);
+
+            // google analytics tracking
+            _gaq.push(['_trackEvent',
+                       'WorkBottom',
+                       'Reached bottom - Loading more data',
+                       'Work',
+                       2,
+                       true]);
         })
         .on('endOfData', function () {
             bottom.dispatch.on('bottom.work', null);
